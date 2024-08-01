1e2dd708ccfd5b7ba4a36b1c3dfe692a3cb74854
harlan.brown@hyland.com <harlan.brown@hyland.com>
2024-08-01 14:48:40 -0500

    2021

--- src/test/java/org/nuxeo/sample/test/TestMyProject.java
+++ src/test/java/org/nuxeo/sample/test/TestMyProject.java
@@ -1,34 +1,42 @@
 package org.nuxeo.sample.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.net.URL;
-
+import javax.inject.Inject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-import org.nuxeo.runtime.test.runner.PartialDeploy;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
-import org.nuxeo.runtime.test.runner.Deploy;
-import org.nuxeo.ecm.platform.test.PlatformFeature;
-import org.nuxeo.ecm.platform.ec.notification.service.NotificationService;
-import org.nuxeo.sample.MyTargetExtension;
+import org.nuxeo.runtime.test.runner.PartialDeploy;
+import org.nuxeo.runtime.test.runner.TargetExtensions;
 
 @RunWith(FeaturesRunner.class)
-@Features({ PlatformFeature.class })
-@Deploy({"org.nuxeo.ecm.platform.notification.core"})                        
-@PartialDeploy(bundle = "studio.extensions.partialdeploytest-suppor", extensions = { MyTargetExtension.class })
-@LocalDeploy({"org.nuxeo.sample.studio.test:OSGI-INF/notification-contrib.xml"})
+@Features(PlatformFeature.class)
+@PartialDeploy(bundle = "studio.extensions.partialdeploytest-suppor", extensions = { TargetExtensions.ContentModel.class })
 public class TestMyProject {
-    @Test
-    public void hello() {
+    @Inject
+    protected CoreSession session;
 
-    	URL localTestTemplate = NotificationService.getTemplateURL("localTestTemplate");
-        assertTrue(localTestTemplate.getFile().endsWith("templates/test.ftl"));
-        
-        URL fromStudioTemplate = NotificationService.getTemplateURL("appReviewStarted");
-        assertTrue(fromStudioTemplate.getFile().endsWith("templates/appReviewStarted.ftl"));
+    @Before
+    public void setUp() {
+        DocumentModel fil1 = session.createDocumentModel("/", "test", "CustomFile");
+        fil1 = session.createDocument(fil1);
+        session.followTransition(new PathRef("test"), "to_archived");
+        session.save();
+    }
+
+    @Test
+    public void run() {
+        DocumentModel doc = session.getDocument(new PathRef("/test"));
+        String lifeCycle = doc.getCurrentLifeCycleState();
+        assertEquals(lifeCycle, "archived"); 
     }
 }
